75d7511f5954d82975b8499245723fe84cb3d26c
gigonelee <gigonelee@ifunfactory.com>
2018-11-30 16:24:26 +0900

    데디케이티드 서버 생성 추가
    Change-Id: I76604afd06c378a09751c0096cc96181dbacc704

--- src/dsm/message_handler.cc
+++ src/dsm/message_handler.cc
@@ -41,6 +41,11 @@ const char *kPlatformName = "platform";
 
 const char *kFacebookAccessToken = "access_token";
 
+// Spawn 관련 키 이름 정의
+const char *kSpawnRequest = "spawn";
+
+const char *kUserData = "user_data";
+
 
 void SendMyMessage(const Ptr<Session> &session,
                    const int64_t code,
@@ -229,11 +234,165 @@ void OnLoginRequest(const Ptr<Session> &session, const Json &message) {
   }
 }
 
+
+void OnDedicatedServerSpawned(const Uuid &match_id,
+                              const std::vector<string> &account_ids,
+                              bool success) {
+  //
+  // 데디케이티드 서버 스폰 결과를 받는 콜백입니다.
+  //
+  // 데디케이티드 서버 매니저는 서버 프로세스 생성을 확인한 후, 매치 데이터를 전송합니다.
+  // 데디케이티드 서버 프로세스가 매치 데이터를 정상적으로 받거나(success)/
+  // 실패하면(not success) 데디케이티드 서버 매니저 가 이 콜백을 호출합니다.
+  //
+  // 데디케이티드 서버 매니저는 이 콜백 함수가 끝나는 즉시 클라이언트에게 데디케이티드
+  // 서버 접속 메시지를 보냅니다. 즉 이 콜백 함수를 호출하는 시점에서 클라이언트는
+  // 데디케이티드 서버와 연결하기 전입니다.
+  //
+
+  std::stringstream account_ids_ss;
+  auto itr = account_ids.begin();
+  auto itr_end = account_ids.end();
+  account_ids_ss << "[";
+  for (; itr != itr_end; ++itr) {
+    if (itr != account_ids.begin()) {
+      account_ids_ss << ", ";
+    }
+    account_ids_ss << "" << *itr;
+  }
+  account_ids_ss << "]";
+
+  LOG(INFO) << "OnDedicatedServerSpawned"
+            << ": match_id=" << match_id
+            << ", account_ids=" << account_ids_ss.str()
+            << ", success=" << (success ? "succeed" : "failed");
+
+  if (not success) {
+    return;
+  }
+
+  //
+  // 매치 ID 를 별도로 저장하거나 관리하려면 이 곳에 코드를 추가해야 합니다.
+  // 또는 클라이언트가 생성한 데디케이티드 서버로 접속하기 전에 처리해야 할 것이
+  // 있을 경우, 이 곳에서 처리해야 합니다.
+}
+
+
+void OnSpawnRequest(const Ptr<Session> &session, const Json &message) {
+  const SessionId &session_id = session->id();
+  const Json &session_context = session->GetContext();
+
+  LOG(INFO) << " OnSpawnRequest"
+            << ": session=" << session_id
+            << ", context=" << session_context.ToString(false)
+            << ", message=" << message.ToString(false);
+
+  //
+  // 데디케이티드 서버 스폰 요청 예제 1 (방 생성)
+  //
+  // 유저 1명을 대상으로 새로운 데디케이티드 서버 프로세스를 생성합니다.
+  // 프로세스 생성 후 다른 유저들은 데디케이티드 서버의 난입 기능을 통해 서버로
+  // 접속해야 합니다.
+  //
+  // 클라이언트는 다음 메시지 형태로 데디케이티드 서버 스폰을 요청해야 합니다.
+  // {
+  //   // Facebook ID 또는 구글+ ID 등 고유한 ID 를 사용해야 합니다.
+  //   "account_id": "id"
+  //   "user_data": {
+  //      ...
+  //   },
+  // }
+
+  // 메시지 안에 필수 파라메터가 있는지 확인합니다.
+  if (not message.HasAttribute(kAccountId, Json::kString) ||
+      not message.HasAttribute(kUserData, Json::kObject) ) {
+    LOG(ERROR) << "The message does not have '" << kAccountId << "' or '"
+               << kUserData << "'"
+               << ": session=" << session_id
+               << ", message=" << message.ToString(false);
+    SendMyMessage(session, 400, "Missing required fields.", Json());
+    return;
+  }
+
+  const string &account_id = message[kAccountId].GetString();
+  const Json &data = message[kUserData];
+
+  //
+  // 데디케이티드 서버 인자 설정 가이드
+  //
+
+  // 1. 매치 ID
+  // 방을 식별하는 용도로 사용합니다.
+  // 아래 함수를 사용하면 스폰 이후 방 정보를 가져올 수 있습니다.
+  // DedicatedServerManager::GetGameState(
+  //     const Uuid &match_id, const GetGameStateCallback &callback);
+  //
+  Uuid match_id = RandomGenerator::GenerateUuid();
+
+  // 2. 매치 데이터
+  // 이 매치에서 사용할 데이터를 넣습니다.
+  // 이 데이터는 각 데디케이티드 서버에서 다음과 같이 받을 수 있습니다.
+  // - 유니티 플러그인:
+  //     - FunapiDedicatedServer.MatchDataCallback 에 등록한 콜백 함수
+  // - 언리얼 엔진 플러그인:
+  //     - SetMatchDataCallback 으로 등록한 콜백 함수
+  //     - GetMatchDataJsonString() 함수
+  Json match_data;
+  match_data["match_type"] = "data 1";
+  match_data["match_bonus_rate"] = 10;
+
+  // 3. 데디케이티드 서버 인자
+  // 데디케이티드 서버 프로세스 실행 시 함께 넘겨 줄 인자를 설정합니다.
+  // 유니티, 언리얼 데디케이티드 서버에서 지원하는 인자가 있거나, 별도로
+  // 인자를 지정해야 할 경우 이 곳에 입력하면 됩니다.
+  const std::vector<string> dedicated_server_args {
+      "-ExtraArg1=value1", "-ExtraArg2=value2", "-ExtraArg3=value3" };
+
+  // 4. account_ids
+  // 데디케이티드 서버 프로세스 생성 시 함께 전달할 계정 ID를 추가합니다.
+  //
+  // 게임 클라이언트는 데디케이티드 서버 프로세스 생성 후 리다이렉션 요청을 받으며
+  // 이후 데디케이티드 서버로 진입하게 됩니다. 데디케이티드 서버는 이 때 클라이언트가
+  // 올바른 account_id 와 token 을 가지고 있는지 다음과 같이 검증할 수 있습니다.
+  // - 유니티 플러그인:
+  //     - FunapiDedicatedServer.AuthUser(string uid, string token)
+  // - 언리얼 플러그인:
+  //     - bool FunapiDedicatedServer::AuthUser(
+  //         const FString& options, const FString& uid_field,
+  //         const FString& token_field, FString &error_message);
+  //
+  std::vector<string> account_ids { account_id };
+
+  // 5. user_data
+  // 데디케이티드 서버 프로세스 생성 시 함께 전달할 유저 데이터를 추가합니다.
+  // 유저 데이터는 각 데디케이티드 서버에서 다음과 같이 받을 수 있습니다.
+  // (uid == account_id 와 동일합니다)
+  // - 유니티 플러그인:
+  //     - FunapiDedicatedServer.UserDataCallback 에 등록한 콜백 함수
+  //     - FunapiDedicatedServer.GetUserDataJsonString(string uid) 함수
+  // 언리얼 플러그인:
+  //     - FunapiDedicatedServer::SetUserDataCallback 으로 등록한 콜백 함수
+  //     - FunapiDedicatedServer::GetUserDataJsonString(const FString &uid) 함수
+  // 각 데디케이티드 서버는 클라이언트가
+  std::vector<Json> user_data { data };
+
+  // 두 컨테이너의 길이가 같아야 합니다. 0번 인덱스의 account_id 는 0번의 user_data
+  // 를 사용하게 됩니다.
+  LOG_ASSERT(account_ids.size() == user_data.size());
+
+  // 준비한 인자들을 넣고 데디케이티드 서버 생성 요청을 합니다.
+  DedicatedServerManager::Spawn(
+      match_id, match_data, dedicated_server_args, account_ids, user_data,
+      bind(&OnDedicatedServerSpawned, _1, _2, _3));
+}
+
 }  // unnamed namespace
 
 void RegisterMessageHandler() {
   // 로그인 요청 핸들러를 등록합니다.
   HandlerRegistry::Register(kLoginRequest, OnLoginRequest);
+  // 데디케이티드 서버 스폰(Spawn) 요청 핸들러를 등록합니다.
+  HandlerRegistry::Register(kSpawnRequest, OnSpawnRequest);
 
 }
 
